aa23ca8b853698db316ca4755cb0e3a9da11bbac
aero <aero_blue@outlook.com>
2019-11-06 11:03:39 -0600

    make functions

--- main.py
+++ main.py
@@ -1,38 +1,129 @@
 import requests
 from bs4 import BeautifulSoup
-from config import username, password
+import config
 from contextlib import closing
-PARSER = 'lxml'
+
+PARSER = "lxml"
+TIMEOUT = 5
+
+
+def get_session_token(session):
+    credentials = {
+        "username": config.username,
+        "password": config.password,
+    }
+    with closing(
+        session.request(
+            "POST",
+            "https://tcu.okta.com/api/v1/authn",
+            json=credentials,
+            timeout=TIMEOUT,
+        )
+    ) as response:
+        response.raise_for_status()
+        session_token = response.json()["sessionToken"]
+        return session_token
+
+
+def get_redirect_url(session):
+    with closing(
+        session.request(
+            "GET", "https://get.cbord.com/tcu/full/login.php", timeout=TIMEOUT
+        )
+    ) as response:
+        response.raise_for_status()
+        redirect_url_tag = BeautifulSoup(response.text, PARSER).select_one(
+            "#fromURI", timeout=TIMEOUT
+        )
+        return redirect_url_tag.get("value")
+
+
+def auth_via_sso(session, session_token, redirect_url):
+    params = {"token": session_token, "redirectUrl": redirect_url}
+    with closing(
+        session.request(
+            "GET",
+            "https://tcu.okta.com/login/sessionCookieRedirect",
+            params=params,
+            timeout=TIMEOUT,
+        )
+    ) as response:
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, PARSER)
+        return [
+            (desired_elem.get("name"), desired_elem.get("value"))
+            for desired_elem in soup.find_all("input")
+        ]
+
+
+def get_funds_home(session):
+    with closing(
+        session.request(
+            "GET", "https://get.cbord.com/tcu/full/funds_home.php", timeout=TIMEOUT
+        )
+    ) as response:
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, PARSER)
+        user_id = (
+            soup.find("option", text="Frog Bucks (refundable)")
+            .get("value")
+            .split(":")[2]
+        )
+        form_token = soup.find(attrs=dict(name="formToken", type="hidden")).get("value")
+        return dict(userId=user_id, formToken=form_token)
+
+
+def get_funds_overview(session, params):
+    with closing(
+        session.request(
+            "POST",
+            "https://get.cbord.com/tcu/full/funds_overview_partial.php",
+            data=params,
+            timeout=TIMEOUT,
+        )
+    ) as response:
+        response.raise_for_status()
+        return BeautifulSoup(response.text, PARSER).find("table").find_all("tr")[1:]
+
+
+def get_current_account_balances(session):
+    session.request(
+        "POST",
+        "https://get.cbord.com/tcu/Shibboleth.sso/SAML2/POST",
+        data=auth_via_sso(
+            session, get_session_token(session), get_redirect_url(session)
+        ),
+        timeout=TIMEOUT,
+    )
+    funds_overview = get_funds_overview(session, get_funds_home(session))
+    return funds_table_to_dict(funds_overview)
+
+
+def funds_table_to_dict(html_table):
+    def get_account_name(row):
+        tags = ["(refundable)", "(non-refundable)"]
+        account_name = row.find("td", class_="first-child account_name").get_text()
+        for tag in tags:
+            if tag in account_name:
+                account_name = account_name[: account_name.find(tag)].strip()
+
+        return account_name
+
+    def get_balance(row):
+        balance = row.find("td", class_="last-child balance").get_text()
+        return balance
+
+    table_dict = {get_account_name(row): get_balance(row) for row in html_table}
+
+    return table_dict
 
 
 def main():
-    ###
-    with requests.Session() as s:
-        with closing(s.get('https://get.cbord.com/tcu/full/login.php')) as r:
-            fromURI = BeautifulSoup(r.text, PARSER).select_one('#fromURI').get('value')
-
-        with closing(s.post('https://tcu.okta.com/api/v1/authn', json=dict(username=username, password=password))) as r:
-            sessionToken = r.json()['sessionToken']
-
-        with closing(s.get('https://tcu.okta.com/login/sessionCookieRedirect', params=dict(token=sessionToken, redirectUrl=fromURI))) as r:
-            data = {desired_elem.get('name'): desired_elem.get('value') for desired_elem in
-                    BeautifulSoup(r.text, PARSER).find_all('input')}
-
-        s.post('https://get.cbord.com/tcu/Shibboleth.sso/SAML2/POST', data=data)  #application/x-www-form-urlencoded
-    ###
-        with closing(s.get('https://get.cbord.com/tcu/full/funds_home.php')) as r:
-            soup = BeautifulSoup(r.text, PARSER)
-            userId = soup.find('option', text='Frog Bucks (refundable)').get('value').split(':')[2]
-            formToken = soup.find(attrs=dict(name='formToken', type='hidden')).get('value')
-
-        with closing(s.post('https://get.cbord.com/tcu/full/funds_overview_partial.php', data=dict(userId=userId, formToken=formToken))) as r:
-            return table_to_dict(BeautifulSoup(r.text, PARSER).find('table').find_all('tr')[1:])
-
-
-def table_to_dict(table):
-    return {row.find('td', class_='first-child account_name').text: row.find('td', class_='last-child balance').text for row in table}
+    with requests.Session() as session:
+        balances = get_current_account_balances(session)
+    for acc, bal in balances.items():
+        print(f"{acc} | Balance: {bal}")
 
 
 if __name__ == "__main__":
-    table = main()
-    print(table)
+    main()
