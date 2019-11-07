79fced4d76a89cb53461d43adb4a3e9a4484e5ba
aero <aero_blue@outlook.com>
2019-11-07 12:44:39 -0600

    add class objects

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ class SessionToken(object):
         credentials = dict(username=config.username, password=config.password)
         with closing(
             s.post(
-                "https://tcu.okta.com/api/v1/authn",
-                json=credentials,
-                timeout=TIMEOUT,
+                "https://tcu.okta.com/api/v1/authn", json=credentials, timeout=TIMEOUT,
             )
         ) as r:
             r.raise_for_status()
@@ -28,14 +26,14 @@ class SessionToken(object):
 class RedirectUrl(object):
     def __init__(self, s):
         with closing(
-            s.get(
-                "https://get.cbord.com/tcu/full/login.php", timeout=TIMEOUT
-            )
+            s.get("https://get.cbord.com/tcu/full/login.php", timeout=TIMEOUT)
         ) as r:
             r.raise_for_status()
-            redirect_url = BeautifulSoup(r.text, PARSER).select_one(
-                "#fromURI", timeout=TIMEOUT
-            ).get("value")
+            redirect_url = (
+                BeautifulSoup(r.text, PARSER)
+                .select_one("#fromURI", timeout=TIMEOUT)
+                .get("value")
+            )
             self.value = redirect_url
 
     def __repr__(self):
@@ -53,8 +51,8 @@ class UserId(object):
 
     def __repr__(self):
         return self.value
-        
-        
+
+
 class FormToken(object):
     def __init__(self, r):
         soup = BeautifulSoup(r.text, PARSER)
@@ -69,16 +67,18 @@ class Table(object):
         with closing(s.get("https://get.cbord.com/tcu/full/funds_home.php")) as r:
             payload = dict(userId=UserId(r), formToken=FormToken(r))
         with closing(
-                s.post(
-                    "https://get.cbord.com/tcu/full/funds_overview_partial.php",
-                    data=payload,
-                    timeout=TIMEOUT,
-                )
+            s.post(
+                "https://get.cbord.com/tcu/full/funds_overview_partial.php",
+                data=payload,
+                timeout=TIMEOUT,
+            )
         ) as response:
             response.raise_for_status()
-        self.html = BeautifulSoup(response.text, PARSER).find("table").find_all("tr")[1:]
+        self.html = (
+            BeautifulSoup(response.text, PARSER).find("table").find_all("tr")[1:]
+        )
         self.dict = self.to_dict()
-        
+
     def to_dict(self):
         def get_account_name(row):
             tags = ["(refundable)", "(non-refundable)"]  # Filter out extraneous ids
@@ -97,25 +97,32 @@ class Table(object):
             get_account_name(row): get_balance(row) for row in self.html
         }  # Formatting dict
         return table_dict
-    
+
     def get_balance(self, account_name):
         return self.dict[account_name]
 
 
 def auth(s):
     with closing(
-            s.get(
-                "https://tcu.okta.com/login/sessionCookieRedirect",
-                params=dict(token=SessionToken(session), redirectUrl=RedirectUrl(session)),
-                timeout=TIMEOUT,
-            )
+        s.get(
+            "https://tcu.okta.com/login/sessionCookieRedirect",
+            params=dict(token=SessionToken(session), redirectUrl=RedirectUrl(session)),
+            timeout=TIMEOUT,
+        )
     ) as response:
         response.raise_for_status()
         soup = BeautifulSoup(response.text, PARSER)
-        return {desired_elem.get("name"): desired_elem.get("value") for desired_elem in soup.find_all("input")}
+        return {
+            desired_elem.get("name"): desired_elem.get("value")
+            for desired_elem in soup.find_all("input")
+        }
 
 
 if __name__ == "__main__":
     with requests.Session() as session:
-        session.post("https://get.cbord.com/tcu/Shibboleth.sso/SAML2/POST", data=auth(session))
-        print(Table(session).get_balance('Meal Plan Frog Bucks'))  # Get an account balance
+        session.post(
+            "https://get.cbord.com/tcu/Shibboleth.sso/SAML2/POST", data=auth(session)
+        )
+        print(
+            Table(session).get_balance("Meal Plan Frog Bucks")
+        )  # Get an account balance
